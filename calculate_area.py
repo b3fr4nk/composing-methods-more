0677f64ed50d0c121035214d0552166b4e58f140
Chris Wood <wchristopherwood912@gmail.com>
2024-02-08 09:09:31 -0800

    all done

---
+++ calculate_area.py
@@ -0,0 +1,34 @@
+# by Kami Bigdely
+# Rename Method
+# Reference: https://parade.com/1039985/marynliles/pick-up-lines/
+
+# TODO: Rename this function to reflect what it's doing.
+def get_area_under_curve(graph):
+    """Calculate the area under the input graph."""
+    # bla bla bla.
+    pass
+
+#####################
+
+
+def get_largest_value(li):
+    m = li[0]
+    for value in li:
+        if value > m:
+            m = value
+    return m
+
+
+li = [5, -1, 43, 32, 87, -100]
+print(get_value(li))
+
+############################
+
+
+# TODO: Rename this function to reflect what it's doing.
+def get_words(sentence):
+    words = sentence[0:].split(' ')
+    return words
+
+
+print(get_words('If you were a vegetable, you’d be a ‘cute-cumber.'))
